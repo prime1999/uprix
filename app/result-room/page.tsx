1c90ee26323a05597ad10ccf5b9b4a0f78355485
Prime <primetechnologies02@gmail.com>
2026-09-08 18:21:42 +0100

    added the connection() to the result room page

--- app/result-room/page.tsx
+++ app/result-room/page.tsx
@@ -1,10 +1,11 @@
+import { connection } from "next/server";
 import ResultRoom from "@/components/result-room/ResultRoom";
 import { redirect } from "next/navigation";
 import { checkResultRoomPaymentStatus } from "@/lib/supabase/result-room";
 
-export const instant = false;
-
 export default async function Room() {
+  await connection();
+
   const status = await checkResultRoomPaymentStatus();
 
   if (!status.isAuthenticated) {
